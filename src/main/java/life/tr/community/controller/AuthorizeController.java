378be0ea1f73c2379c34bb7edb65fe6018cad116
SFY-GZY <896829281@qq.com>
2020-02-09 17:24:22 +0800

    properties

--- src/main/java/life/tr/community/controller/AuthorizeController.java
+++ src/main/java/life/tr/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.tr.community.dto.AccessTokenDTO;
 import life.tr.community.dto.GithubUser;
 import life.tr.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,22 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github..redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state
     ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("5482368eec1bd7f16a0b");
-        accessTokenDTO.setClient_secret("ee059523697b41b837ac1c65a18002e0730bd3e4");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
