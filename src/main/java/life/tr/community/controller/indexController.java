5cba25909370e55701579e685f2c450994364295
SFY-GZY <896829281@qq.com>
2020-02-14 17:20:37 +0800

    profile html

--- src/main/java/life/tr/community/controller/indexController.java
+++ src/main/java/life/tr/community/controller/indexController.java
@@ -1,5 +1,6 @@
 package life.tr.community.controller;
 
+import life.tr.community.dto.PaginationDTO;
 import life.tr.community.dto.QuestionDTO;
 import life.tr.community.mapper.QuestionMapper;
 import life.tr.community.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +28,9 @@ public class indexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size
     ){
         Cookie[] cookies = request.getCookies();
         if (cookies !=null&& cookies.length !=0)
@@ -41,8 +45,8 @@ public class indexController {
             }
         }
 
-        List<QuestionDTO> questionList=questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination=questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
