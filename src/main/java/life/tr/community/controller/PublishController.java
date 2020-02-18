a91720a3d6b03879c8f56a0ff36a937f84c3fde6
SFY-GZY <896829281@qq.com>
2020-02-18 12:23:47 +0800

    BiaoQian

--- src/main/java/life/tr/community/controller/PublishController.java
+++ src/main/java/life/tr/community/controller/PublishController.java
@@ -1,11 +1,13 @@
 package life.tr.community.controller;
 
+import life.tr.community.cache.TagCache;
 import life.tr.community.dto.QuestionDTO;
 import life.tr.community.mapper.QuestionMapper;
 import life.tr.community.mapper.UserMapper;
 import life.tr.community.model.Question;
 import life.tr.community.model.User;
 import life.tr.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,12 +32,14 @@ public class PublishController {
         model.addAttribute("description",question.getDescription());
         model.addAttribute("tag",question.getTag());
         model.addAttribute("id",question.getId());
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish( Model model){
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -52,6 +56,8 @@ public class PublishController {
         model.addAttribute("title",title);
         model.addAttribute("description",description);
         model.addAttribute("tag",tag);
+        model.addAttribute("tags", TagCache.get());
+
         if(title ==null|| title ==""){
             model.addAttribute("error","标题不能为空");
             return "publish";
@@ -64,6 +70,13 @@ public class PublishController {
             model.addAttribute("error","标签不能为空");
             return "publish";
         }
+
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)){
+            model.addAttribute("error","输入非法标签" +invalid);
+            return "publish";
+        }
+
         User user = (User)request.getSession().getAttribute("user");
         if(user == null){
             model.addAttribute("error","用户未登录");
